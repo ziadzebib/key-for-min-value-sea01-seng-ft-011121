d1eac1851c91381232972dc7f53e356e8e4eb7f4
Ziad Zebib <zebibziad@gmail.com>
2020-12-31 21:42:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = 
+  min_value = 10000000000
   name_hash.each do |key, value|
     if value < min_value do 
       min_value = value 
