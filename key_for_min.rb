417a614dc9fce5ece93c235a56e9bbbcf96bdfc6
Ziad Zebib <zebibziad@gmail.com>
2020-12-31 21:45:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = 10000000000
   name_hash.each do |key, value|
-    if value < min_value do 
+    if value < min_value
       min_value = value 
       min_key = key
     end
